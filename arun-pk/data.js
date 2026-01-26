2fa5d57082a5f6bd3d6b81f20820b608a3fe059b
logicgates-tech <we.logicgates@gmail.com>
2026-01-26 22:12:26 +0530

    Create data.js

---
+++ arun-pk/data.js
@@ -0,0 +1,15 @@
+const profile = {
+  name: "Arun P.K",
+  title: "Operations Manager",
+  company: "Stone Lodge’s – APR Group Of Companies",
+
+  phone: "+917356455578",
+  displayPhone: "+91 73564 55578",
+  whatsapp: "917356455578",
+
+  emailWork: "arun@aprinfra.org",
+  emailPersonal: "arunidinfo@gmail.com",
+
+  locationText: "Kainatty, Kalpetta, Wayanad",
+  locationUrl: "https://maps.google.com?q=Kainatty,Kalpetta,Wayanad,Kerala"
+};
