b19acf0cba51b063d2c06239824d5f12f223c1e5
sanal <sanalbathery@gmail.com>
2026-02-05 19:14:38 +0530

    abc

---
+++ T/tms/data.js
@@ -0,0 +1,22 @@
+const profile = {
+  name: "Thomas Mathew",
+  title: "Manager",
+  company: "ABC Hardwares",
+
+  phone: "+919847050485",
+  displayPhone: "+919847050485",
+  whatsapp: "+919847050485",
+
+  emailWork: "thomas.mathew@gmail.com",
+  emailPersonal: "arunidinfo@gmail.com",
+
+  website: {
+    url: "https://sites.google.com/view/abchardware/home",
+    label: "ABC Hardwares"
+  },
+
+  address: "Sulthan Bathery, Kalpetta, Mananthavady",
+
+  locationText: "Sulthan Bathery, Wayanad",
+  locationUrl: "https://maps.app.goo.gl/7FFwqChduaWfTgEV6"
+};
